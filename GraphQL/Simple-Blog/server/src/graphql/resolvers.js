af90333c79991610eeb0174ae65d28fd8f056fb1
Aljith KJ <kwdeathlok@gmail.com>
2024-02-08 22:09:33 +0530

    add controllers in the resolvers

--- GraphQL/Simple-Blog/server/src/graphql/resolvers.js
+++ GraphQL/Simple-Blog/server/src/graphql/resolvers.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
-import { loginUser, registerUser } from '../controllers/authController.js';
-import { createPost } from '../controllers/postController.js';
+import { getUser, loginUser, registerUser } from '../controllers/authController.js';
+import { createPost, getPostById, getPosts } from '../controllers/postController.js';
 
 const prisma = new PrismaClient();
 
@@ -9,26 +9,15 @@ export const resolvers = {
         async posts(_, __, context) {
             const id = context.req.user.id;
 
-            return await prisma.post.findMany({
-                where: {
-                    userId: {
-                        not: id
-                    }
-                }
-            });
+            return await getPosts({ id });
         },
         async post(_, { id }) {
-            const post = await prisma.post.findUnique({
-                where: {
-                    id: Number(id)
-                }
-            })
-
-            if(!post) {
-                throw new Error('No such post exist!');
-            }
-
-            return post;
+            return await getPostById({ id });
+        }
+    },
+    Post: {
+        async user(parent) {
+            return await getUser({ id: parent.userId });
         }
     },
     Mutation: {
